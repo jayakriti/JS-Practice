482c5074a5d5909c8571ca7065cc3a347332ce90
Jaya Kriti <jayakriti017@gmail.com>
2026-05-15 11:28:47 +0000

    datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 457891;
+let accEmailId = "abc@xyz.com";
+var accPassword = "6419865";
+accCity = "Bangalore";
+let accState;
+
+console.log(accountId);
+console.table([accountId, accEmailId, accPassword, accCity, accState])
