ff587cfd70822577c63870a3cf5650ceba6a5bed
Jaya Kriti <jayakriti017@gmail.com>
2026-05-20 15:06:20 +0000

    halft summary of datatypes

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive Data Types
+
+// 7 types: Number, String, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+const bigNumber = 65192782756274n;
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
