8ab3cabce2d227f06e408039ea282ce840d25fc6
Jaya Kriti <jayakriti017@gmail.com>
2026-05-24 06:42:28 +0000

    number

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,13 @@
+const score = 400;
+console.log(score);
+console.log(typeof score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(typeof balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const anotherNumber = 123.8165187;
+console.log(anotherNumber.toPrecision(3));
