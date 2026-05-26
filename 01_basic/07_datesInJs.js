c926180c3e42f2487cc1b1de48b960db8bda2d86
Jaya Kriti <jayakriti017@gmail.com>
2026-05-26 11:25:41 +0000

    date complete

--- 01_basic/07_datesInJs.js
+++ 01_basic/07_datesInJs.js
@@ -11,3 +11,21 @@ console.log(myDate.toLocaleTimeString());
 
 let myCreatedDate = new Date(2026, 0, 18); //months counting starts from 0 like an ARRAY count
 console.log(myCreatedDate.toDateString());
+
+let newCreatedDate = new Date("01-18-2026"); // MM-DD-YYYY formate
+console.log(newCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp); // display time in Milisecond
+console.log(newCreatedDate.getTime()); // display time in Milisecond for that Date
+console.log(Math.floor(Date.now()/1000)); //display time in second
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth() + 1); //It counts month from 0 so adding 1 to make the result normal
+console.log(newDate.getDay());
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long"
+})) // display the day in long formate as "Monday" or, "Tuesday"
