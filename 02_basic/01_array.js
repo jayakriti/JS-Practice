4c58a288fa98af1fdbdc2a7ef0255c5987c4212d
Jaya Kriti <jayakriti017@gmail.com>
2026-05-26 11:58:26 +0000

    array start

---
+++ 02_basic/01_array.js
@@ -0,0 +1,6 @@
+const myArr = [0, 1, 2, 3, 4, 5]; // numeric value array
+const myHeros = ["abc", "def", "ghi"]; //string value array
+
+const myArr2 = new Array(1, 2, 3, 4, 5); //declaring array in object format
+
+console.log(myArr[0]);
