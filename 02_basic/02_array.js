c31998dd4a5057985be96f30bbc97f886ce4419c
Jaya Kriti <jayakriti017@gmail.com>
2026-05-30 11:05:04 +0000

    array

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -15,6 +15,19 @@ console.log(allHeros);
 const all_new_heros = [...marvel_heros1, ...dc_heros1]; //spread operator(...) also adds multiple array and returns new array
 console.log(all_new_heros);
 
-// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
-// const real_another_array = another_array.flat(infinity);
-// console.log(real_another_array);
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+const real_another_array = another_array.flat(Infinity); // idoly we should always give numeric value as depth instead of infinity like 2 or 3 or ...
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Jaya"));
+console.log(Array.from("Jaya"));
+console.log(Array.from({name: "Jaya"})); // In this case of OBJECT, we need to tell which part has to be converted into ARRAY, key or value. Else it will always relult in empty array if it fails to create the array
+console.log("=>  ", Array.from([1, 2, 3], (x) => (2 * x)))
+
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
