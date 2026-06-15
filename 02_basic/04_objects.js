cff625959577ef36126cfe939a0e3c9f70fdb9bf
Jaya Kriti <jayakriti017@gmail.com>
2026-06-15 12:13:23 +0000

    obj2

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -1,4 +1,21 @@
 //const frndMake = new Object();
 
 const frndMake = {};
+
+frndMake.id = "123abc";
+frndMake.name = "Rahul";
+frndMake.isLoggedIn = false;
+
 console.log(frndMake);
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname: {
+        userfullname: {
+            firstName: "Jaya",
+            lastName: "Kriti"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstName);
