5fc890533eca5a670fba629004bbd0666827bb85
Jaya Kriti <jayakriti017@gmail.com>
2026-07-13 11:29:34 +0000

    while, do-while

---
+++ 05_iterations/02_while_doWhile.js
@@ -0,0 +1,35 @@
+// =========================== WHILE LOOP =========================== //
+let index = 0;
+while(index <= 10)
+{
+    console.log(`Value of Index is: ${index}`);
+    index = index+2;
+}
+
+console.log("------------------------------------------");
+
+let myArray = ["Jaya", "Kriti", "Roma", "Rahul", "Rudra"];
+let i = 0;
+while(i < myArray.length)
+{
+    console.log(`Value is ${myArray[i]}`);
+    i++;
+}
+
+console.log("------------------------------------------");
+
+// =========================== DO-WHILE LOOP =========================== //
+
+{let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);}
+
+console.log("------------------------------------------");
+
+{let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);}
